433c8a56b2432e53db0f2982ae58c098804f7e0c
NamPhuThuy <ttnam957@gmail.com>
2024-11-03 00:27:41 +0700

    Add: some scriptableObjects, Modify: change to Android platform

--- Assets/_GamePrototype2D/ScriptableObjects/PlayerWeaponDataList.cs
+++ Assets/_GamePrototype2D/ScriptableObjects/PlayerWeaponDataList.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerWeaponDataList", menuName = "ScriptableObjects/PlayerWeaponDataList")]
-public class PlayerWeaponDataList : ScriptableObject
+public class WeaponDataListPlayer : ScriptableObject
 {
-    public List<PlayerWeaponData> PlayerWeaponDatas;
+    public List<WeaponDataPlayer> PlayerWeaponDatas;
+}
+
+[Serializable]
+public class WeaponDataPlayer
+{
+    public string id;
+    public int amount = 0;
+    public int level;
 }
--- Assets/_GamePrototype2D/ScriptableObjects/WeaponDataList.cs
+++ Assets/_GamePrototype2D/ScriptableObjects/WeaponDataList.cs
@@ -1,32 +1,33 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WeaponDataList", menuName = "ScriptableObjects/WeaponDataList")]
+[CreateAssetMenu(fileName = "WeaponDataListGame", menuName = "ScriptableObjects/WeaponDataListGame")]
 
-public class WeaponDataList : ScriptableObject
+public class WeaponDataListGame : ScriptableObject
 
 {
     public List<WeaponData> WeaponDatas;
 }
 
-public class PlayerWeaponData : ScriptableObject
-{
-    public int amount = 0;
-    public string id;
-}
-
-
-public class WeaponData : ScriptableObject
+[Serializable]
+public class WeaponData
 {
     public string name;
     public string id;
     public string description;
-    public int level;
     public Rarity rarity;
     public AttackType attackType;
+    public List<WeaponStats> weaponStatsList;
+}
+
+[Serializable]
+public class WeaponStats
+{
     public float attackSpeed;
     public float power; // Damage multiplier
     public int damage;
+    public Sprite image;
 }
 
 public enum Rarity
@@ -37,6 +38,7 @@ public enum Rarity
     Epic,
     Legendary
 }
+
 
 public enum AttackType
 {
